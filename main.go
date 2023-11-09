2ab4cfdb210942c104664031a3626f73600cbaab
Nathaniel Macapinlac <nlm040603@gmail.com>
2023-11-09 01:07:50 -0500

    refactor need to fix pause

--- main.go
+++ main.go
@@ -8,8 +8,17 @@ import (
 )
 
 type Paused struct {
-	isPaused bool
-	text     string
+	isPaused       bool
+	text           string
+	pauseStartTime time.Time
+	pauseDuration  time.Duration
+}
+
+func initPaused() Paused {
+	return Paused{
+		isPaused: false,
+		text:     "to unpause the timer",
+	}
 }
 
 type Timer struct {
@@ -21,52 +30,52 @@ type Timer struct {
 
 func initTimer() Timer {
 	return Timer{
-		studyDuration:     25 * time.Minute,
-		breakDuration:     5 * time.Minute,
-		longBreakDuration: 15 * time.Minute,
+		studyDuration:     5 * time.Second,
+		breakDuration:     4 * time.Second,
+		longBreakDuration: 5 * time.Second,
 		Pause:             initPaused(),
-	}
-}
-
-func initPaused() Paused {
-	return Paused{
-		isPaused: true,
-		text:     "to unpause the timer",
 	}
 }
 
 func main() {
 	const (
-		screenWidth   = int32(400)
-		screenHeight  = int32(200)
-		studyDuration = 25 * time.Minute
+		screenWidth  = int32(400)
+		screenHeight = int32(200)
 	)
 
 	var (
-		timer   Timer
-		endTime time.Time
+		timer         Timer
+		endTime       time.Time
+		remainingTime time.Duration
 	)
 
 	timer = initTimer()
 	endTime = time.Now().Add(timer.studyDuration)
+	remainingTime = endTime.Sub(time.Now())
 
 	rl.InitWindow(screenWidth, screenHeight, "Pomodoro")
 
 	for !rl.WindowShouldClose() {
+		//timer methods
 		timer.changePauseStatus()
-		remainingTime := endTime.Sub(time.Now())
+		timer.changeText()
+		timer.startPauseTimer()
+
+		minutes := int(remainingTime.Minutes())
+		seconds := int(remainingTime.Seconds()) % 60
+
 		statusText := "STUDY"
-		timeText := fmt.Sprintf("%.001f", remainingTime.Seconds()/60)
+		timeText := fmt.Sprintf("%02d:%02d", minutes, seconds)
 		pauseText := fmt.Sprintf("Press [SPACE]: %s", timer.Pause.text)
 
 		statusTextSize := rl.MeasureText(statusText, 20)
 		timeTextSize := rl.MeasureText(timeText, 150)
 		pauseTextSize := rl.MeasureText(pauseText, 20)
 
-		if !timer.Pause.isPaused {
-			endTime = time.Now().Add(timer.studyDuration)
-		} else {
+		if timer.Pause.isPaused {
 			remainingTime = endTime.Sub(time.Now())
+			timer.Pause.pauseDuration = time.Since(timer.Pause.pauseStartTime)
+		} else {
 		}
 
 		rl.BeginDrawing()
@@ -86,10 +95,20 @@ func (t *Timer) changePauseStatus() {
 	if rl.IsKeyPressed(rl.KeySpace) {
 		t.Pause.isPaused = !t.Pause.isPaused
 	}
+}
 
+func (t *Timer) changeText() {
 	if t.Pause.isPaused {
 		t.Pause.text = "to unpause the timer"
 	} else {
 		t.Pause.text = "to pause the timer"
+	}
+}
+
+func (t *Timer) startPauseTimer() {
+	if t.Pause.isPaused {
+
+	} else {
+
 	}
 }
